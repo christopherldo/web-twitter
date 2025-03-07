7aa675ebbb09c975a47d863744a461090f804a43
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-03-06 21:37:11 -0400

    add signin page and form component with navigation link to signup

---
+++ src/components/auth/signin-form.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export const SigninForm = () => {
+  const router = useRouter();
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleButtonEntrar = () => {
+    router.replace("/home");
+  };
+
+  return (
+    <>
+      <input
+        name="email"
+        type="email"
+        placeholder="Digite seu e-mail"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Digite sua senha"
+        value={form.password}
+        onChange={handleChange}
+      />
+
+      <button onClick={handleButtonEntrar}>Entrar</button>
+    </>
+  );
+};
