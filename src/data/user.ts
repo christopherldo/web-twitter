21fb86109b89b35f87155167f7efafb989b640b4
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-03-09 10:13:38 -0400

    add user model and sample user data

---
+++ src/data/user.ts
@@ -0,0 +1,12 @@
+import { User } from "@/models/User";
+
+export const user: User = {
+  slug: "ChristopherLDO",
+  name: "Christopher de Oliveira",
+  avatar:
+    "https://ui-avatars.com/api?background=random&name=Christopher&size=100",
+  cover: "https://picsum.photos/1920/1080",
+  bio: "Desenvolvedor FullStack há 4 anos.",
+  link: "https://chrisldo.com",
+  postCount: 127,
+};
---
+++ src/models/User.ts
@@ -0,0 +1,9 @@
+export interface User {
+  slug: string;
+  name: string;
+  avatar: string;
+  cover?: string;
+  bio?: string;
+  link?: string;
+  postCount?: number;
+}
