dd49939eeaf3918469db83a6b153520cddf9bb93
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-03-09 10:01:05 -0400

    add navigation layout with home and profile links

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -1,3 +1,8 @@
+import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
+
+import { NavItem } from "@/components/nav/nav-item";
+import { Logo } from "@/components/ui/logo";
+
 interface LayoutProps {
   children: React.ReactNode;
 }
@@ -5,8 +10,15 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
-      <section className="hidden lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
-        ESQUERDA
+      <section className="flex lg:flex flex-col sticky top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
+        <div className="flex-1 mt-6">
+          <Logo size={24} />
+          <nav className="mt-11">
+            <NavItem href="/home" icon={faHouse} label="Página inicial" />
+            <NavItem href="/profile" icon={faUser} label="Meu perfil" />
+          </nav>
+        </div>
+        <div>...</div>
       </section>
       <section className="flex-1 max-w-lg">{children}</section>
       <aside className="hidden lg:flex flex-col gap-6 sticky top-0 h-fit w-96 px-8 py-6 border-l-2 border-gray-900">
