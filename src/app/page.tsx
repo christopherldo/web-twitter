33c1e05a7438be9ef117d72998cb88891d8513a6
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-03-09 09:51:15 -0400

    add home page and layout components with redirection logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { Logo } from "@/components/ui/logo";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return <div>...</div>;
+  setTimeout(() => {
+    redirect("/home");
+  }, 2000);
+
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Logo size={80} />
+    </div>
+  );
 }
