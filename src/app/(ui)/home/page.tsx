795c236ea6db931570ea777018d50099583cad73
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-03-11 19:24:39 -0400

    add HomeHeader and HomeMenu components to enhance page layout

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,3 +1,5 @@
+import { HomeHeader } from "@/components/home/home-header";
+
 export default function Page() {
-  return <div>...</div>;
+  return <HomeHeader />;
 }
---
+++ src/components/home/home-menu.tsx
@@ -0,0 +1,7 @@
+interface HomeMenuProps {
+  closeAction: () => void;
+}
+
+export const HomeMenu = ({ closeAction }: HomeMenuProps) => {
+  return <div>...</div>;
+};
