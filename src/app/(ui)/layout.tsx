f3fcf5e16d530766c2176deb0cb5c581f22f026b
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-03-11 19:02:12 -0400

    add trending area component to layout with trending items display

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -5,6 +5,7 @@ import { Logo } from "@/components/ui/logo";
 import { NavLogout } from "@/components/nav/nav-logout";
 import { NavMyProfile } from "@/components/nav/nav-myprofile";
 import { SearchInput } from "@/components/ui/search-input";
+import { TrendingArea } from "@/components/ui/trending-area";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -29,6 +30,7 @@ export default function Layout({ children }: LayoutProps) {
       <section className="flex-1 max-w-lg">{children}</section>
       <aside className="flex lg:flex flex-col gap-6 sticky top-0 h-fit w-96 px-8 py-6 border-l-2 border-gray-900">
         <SearchInput hideOnSearch />
+        <TrendingArea />
       </aside>
     </main>
   );
