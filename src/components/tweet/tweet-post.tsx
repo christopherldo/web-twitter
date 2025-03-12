f8f143e1e896d250215ad09efd772329c2002dcb
Christopher Leonardo de Oliveira <christopherldo@gmail.com>
2025-03-12 19:25:18 -0400

    add TweetPost component to enable user tweet creation and integrate it into the home page

---
+++ src/components/tweet/tweet-post.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { user } from "@/data/user";
+import { Button } from "../ui/button";
+
+export const TweetPost = () => {
+  const handleImageUpload = () => {};
+  const handlePostClick = () => {};
+
+  return (
+    <div className="flex gap-6 px-8 py-6 border-b-2 border-gray-900">
+      <div>
+        <Image
+          src={user.avatar}
+          alt={user.name}
+          className="size-12 rounded-full"
+          width={48}
+          height={48}
+        />
+      </div>
+      <div className="flex-1">
+        <div
+          className="inline-flex min-h-14 outline-none text-lg text-white empty:before:text-gray-500 empty:before:content-[attr(data-placeholder)]"
+          contentEditable
+          role="textbox"
+          data-placeholder="O que está acontecendo?"
+        />
+        <div className="flex justify-between items-center mt-2">
+          <div onClick={handleImageUpload} className="cursor-pointer">
+            <FontAwesomeIcon icon={faImage} className="size-6" />
+          </div>
+          <div className="w-28">
+            <Button label="Postar" size="medium" onClick={handlePostClick} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
